6923cd6d89915337d76b995327ee420ec4ddb243
Carlos Lins <lins.carlinhos@gmail.com>
2022-04-07 12:10:32 -0300

    Commit back-end no front

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,53 @@
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      try {
+        await fauna.query(
+
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email),
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { user }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email),
+              )
+            )
+            ),
+        )
+          return true
+
+      } catch(err) {
+        console.log(err)
+        return false;
+      }
+    }
+      
+    },
+})
